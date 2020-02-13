fd5c21534af4d56e3745fbbf320c7bc74aa8409f
wnpraf <rapribu@gmail.com>
2020-02-13 17:21:15 +0100

    Fix empty array issue

--- src/components/SlideTracker.js
+++ src/components/SlideTracker.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default ({slides,slidePosition}) => {
 
   return <div className="slide-tracker flex justify-center items-center">
-    {[].fill(slides).map((e,i) => {
+    {Array(slides).fill(null).map((e,i) => {
       return <span key={i} className={`w-2 h-2 mr-2 rounded-full border border-gray-200 ${ slidePosition === i ? 'bg-gray-200' : ''}`}></span>
     })}
   </div>
