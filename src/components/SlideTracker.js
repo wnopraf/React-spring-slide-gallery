531d7dbb70d513f8901738b323437090a1d353c2
wnpraf <rapribu@gmail.com>
2020-02-13 16:41:40 +0100

    Rename props

--- src/components/SlideTracker.js
+++ src/components/SlideTracker.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default ({slideNumber,slidePosition}) => {
+export default ({slides,slidePosition}) => {
 
   return <div className="flex justify-center items-center">
-    {[].fill(3).map((e,i) => {
+    {[].fill(slides).map((e,i) => {
       return <span key={i} className={`w-2 h-2 mr-2 rounded-full border border-gray-200 ${ slidePosition === i ? 'bg-gray-200' : ''}`}></span>
     })}
   </div>
