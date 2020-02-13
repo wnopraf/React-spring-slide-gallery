66415f966e3d7df39f8b9a6b0cf4108cf4394ac0
wnpraf <rapribu@gmail.com>
2020-02-13 16:59:59 +0100

    Add slide-tracker class

--- src/components/SlideTracker.js
+++ src/components/SlideTracker.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default ({slides,slidePosition}) => {
 
-  return <div className="flex justify-center items-center">
+  return <div className="slide-tracker flex justify-center items-center">
     {[].fill(slides).map((e,i) => {
       return <span key={i} className={`w-2 h-2 mr-2 rounded-full border border-gray-200 ${ slidePosition === i ? 'bg-gray-200' : ''}`}></span>
     })}
